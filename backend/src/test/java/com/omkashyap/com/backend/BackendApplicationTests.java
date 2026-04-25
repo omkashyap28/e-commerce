d5b6d8477f1ea44f4c0691ed6337ba3cf4b23516
Hariom Kashyap <omkashyap6870@gmail.com>
2026-04-25 14:52:44 +0530

    initial commit

---
+++ backend/src/test/java/com/omkashyap/com/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omkashyap.com.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
