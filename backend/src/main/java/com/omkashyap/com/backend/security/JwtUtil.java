81f4d410bbb604c96c593ba309c373deb86a4ef0
Hariom Kashyap <omkashyap6870@gmail.com>
2026-05-04 00:27:47 +0530

    updating jwt methods

--- backend/src/main/java/com/omkashyap/com/backend/security/JwtUtil.java
+++ backend/src/main/java/com/omkashyap/com/backend/security/JwtUtil.java
@@ -1,14 +1,14 @@
 package com.omkashyap.com.backend.security;
 
 import com.omkashyap.com.backend.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -17,17 +17,28 @@ public class JwtUtil {
   @Value("${jwt.secretKey}")
   private String SECRET;
 
+  private static final long EXPIRATION_TIME = 86400000;
+
   private SecretKey getSecretKey() {
-    return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 
   public String generateAccessToken(User user) {
     return Jwts.builder()
-        .setSubject(user.getEmail())
-        .claim("userId", user.getPublicId())
-        .setIssuedAt(new Date())
-        .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-        .signWith(SignatureAlgorithm.HS256, getSecretKey())
+        .subject(user.getEmail())
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+        .signWith(getSecretKey())
         .compact();
+  }
+
+  public String getUserEmailFromToken(String token) {
+    return Jwts.parser()
+        .verifyWith(getSecretKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload()
+        .getSubject();
   }
 }
