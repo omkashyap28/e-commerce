3ec4bcb262879d7210482fb721166ce9f6823214
Hariom Kashyap <omkashyap6870@gmail.com>
2026-05-04 22:58:29 +0530

    address storing feature

--- backend/src/main/java/com/omkashyap/com/backend/service/UserService.java
+++ backend/src/main/java/com/omkashyap/com/backend/service/UserService.java
@@ -1,7 +1,11 @@
 package com.omkashyap.com.backend.service;
 
+import com.omkashyap.com.backend.dto.requestDto.UserAddressRequestDto;
+import com.omkashyap.com.backend.dto.responseDto.AllAddressResponseDto;
+import com.omkashyap.com.backend.dto.responseDto.UserAddressResponseDto;
 import com.omkashyap.com.backend.dto.responseDto.UserResponseDto;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserService {
@@ -10,4 +14,8 @@ public interface UserService {
   UserResponseDto updatePartialUserDetails(String userId, Map<String, Object> updates);
 
   void deleteUserById(String userId);
+
+  UserResponseDto addAddressToUser(String userId, UserAddressRequestDto addressRequestDto);
+
+  List<AllAddressResponseDto> getAllAddressByUserId(String userId);
 }
