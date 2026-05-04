22e200b02bbb7cbca186b6c264e8ef1a0cc95416
Hariom Kashyap <omkashyap6870@gmail.com>
2026-05-04 22:57:19 +0530

    address storing feature

--- backend/src/main/java/com/omkashyap/com/backend/controller/UserController.java
+++ backend/src/main/java/com/omkashyap/com/backend/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.omkashyap.com.backend.controller;
 
+import com.omkashyap.com.backend.dto.requestDto.UserAddressRequestDto;
+import com.omkashyap.com.backend.dto.responseDto.AllAddressResponseDto;
 import com.omkashyap.com.backend.dto.responseDto.UserResponseDto;
+import com.omkashyap.com.backend.repository.AddressRepository;
+import com.omkashyap.com.backend.service.AddressService;
 import com.omkashyap.com.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,6 +21,7 @@ import java.util.Map;
 public class UserController {
 
   private final UserService userService;
+  private final AddressService addressService;
 
   @GetMapping("/{userId}")
   ResponseEntity<UserResponseDto> getUserById(@PathVariable("userId") String userId) {
@@ -32,5 +38,23 @@ public class UserController {
     userService.deleteUserById(userId);
     return ResponseEntity.noContent().build();
   }
+
+  @PostMapping("/{userId}/address")
+  ResponseEntity<UserResponseDto> createAddressByUserId(@PathVariable String userId, @RequestBody UserAddressRequestDto addressRequestDto) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(userService.addAddressToUser(userId, addressRequestDto));
+  }
+
+  @GetMapping("/{userId}/address")
+  ResponseEntity<List<AllAddressResponseDto>> getAddressByUserId(@PathVariable String userId) {
+    return ResponseEntity.status(HttpStatus.OK).body(userService.getAllAddressByUserId(userId));
+  }
+
+//  @DeleteMapping("/{userId}/address")
+//  ResponseEntity<Void> deleteAddressByUserId(@PathVariable String userId) {
+//    addressService.deleteAddressByUserId(userId);
+//    return ResponseEntity.noContent().build();
+//  }
+
+
 
 }
